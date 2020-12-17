3b919fab81aea8218355f58b90449ebc497af6b1
binhnv1986 <64832986+binhnv1986@users.noreply.github.com>
2020-12-17 09:46:19 +0700

    Create lvmubuntu.sh

---
+++ lvmubuntu.sh
@@ -0,0 +1,23 @@
+#lvm.sh nẳm trong /usr/local/bin
+#!/bin/bash
+PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
+
+fdisk /dev/sda <<EOF
+n
+p
+4
+
+
+t
+4
+8e
+w
+EOF
+
+partprobe
+pvcreate /dev/sda4
+vgextend ubuntu-vg /dev/sda4
+lvextend -l +100%FREE /dev/ubuntu-vg/root /dev/sda4
+resize2fs /dev/ubuntu-vg/root
+
+rm -rf /var/spool/cron/crontabs/root
