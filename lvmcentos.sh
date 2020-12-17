ed549bbee91a259e8b191156f1879129474e61bc
binhnv1986 <64832986+binhnv1986@users.noreply.github.com>
2020-12-17 09:45:44 +0700

    Create lvmcentos.sh

---
+++ lvmcentos.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
+
+fdisk /dev/sda <<EOF
+n
+p
+3
+
+
+t
+3
+8e
+w
+EOF
+
+partprobe
+pvcreate /dev/sda3
+vgextend centos /dev/sda3
+lvextend -l +100%FREE /dev/centos/root /dev/sda3
+xfs_growfs /dev/centos/root
+
+rm -rf /var/spool/cron/root
